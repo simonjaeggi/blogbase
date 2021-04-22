00e7d432d00475828c7a1b4260835a91517cb040
Simon Jäggi <dev.simon.jaeggi@gmail.com>
2021-04-22 16:55:13 +0200

    added user index and show page. Shared article index form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,45 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
+
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Welcome to blogbase #{@user.username}!"
+			flash[:success] = "Welcome to blogbase #{@user.username}"
 			redirect_to articles_path
 		else
 			render 'new'
 		end
 	end
-    
-    private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:success] = "Your account was updated successfully"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+
 end
