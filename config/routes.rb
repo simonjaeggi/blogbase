309c606f9548f7f7a69eeb521a44dad6853015b4
Simon Jäggi <dev.simon.jaeggi@gmail.com>
2021-04-24 23:28:35 +0200

    startet creating routes, controller and session files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  resources :articles
+  
   root 'pages#index'
   get 'about', to: 'pages#about'
-  resources :articles
+  get 'login', to: 'sessions#new'
   get 'signup', to: 'users#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users, except: [:new]
+ 
+
 end
